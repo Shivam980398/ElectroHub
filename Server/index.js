5c244d71ffb109cafbaafc71f16a7b6c278800a8
shivam980398 <shivayadu89@gmail.com>
2025-07-19 16:30:02 +0530

    allowing servers for any ip address or site

--- Server/index.js
+++ Server/index.js
@@ -9,11 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // for parsing application/json
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://electrohubs.netlify.app",
-    ],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 204,
